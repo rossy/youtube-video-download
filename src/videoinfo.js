68e6063a32819e402c55f112efece9c0a50199b8
James Ross-Gowan <rossymiles@gmail.com>
2012-12-09 14:34:54 +1100

    Add upload date to VideoInfo

--- src/videoinfo.js
+++ src/videoinfo.js
@@ -44,6 +44,23 @@ var VideoInfo = (function() {
 				return document.querySelector("span[itemprop=author] > link[itemprop=url]").getAttribute("href").match(/www.youtube.com\/user\/([^\/]+)/)[1];
 			}
 		);
+
+		self.date = Try.all(
+			function() {
+				return new Date(document.getElementById("eow-date").textContent);
+			}
+		);
+
+		if (self.date)
+		{
+			self.day = ("0" + self.date.getDate()).match(/..$/)[0];
+			self.month = ("0" + (self.date.getMonth() + 1)).match(/..$/)[0];
+			self.year = self.date.getFullYear().toString();
+
+			self.date.toString = function() {
+				return self.year + "-" + self.month + "-" + self.day;
+			};
+		}
 	}
 
 	return self;
--- youtube-video-download.user.js
+++ youtube-video-download.user.js
@@ -63,7 +63,7 @@
  function script()
  {
 
-  var version = "4.0.3", hash = "acfc767";
+  var version = "4.0.3", hash = "b429188";
 // -- Object tools --
 // has(obj, key) - Does the object contain the given key?
 var has = Function.call.bind(Object.prototype.hasOwnProperty);
@@ -298,6 +298,20 @@ var VideoInfo = (function() {
     return document.querySelector("span[itemprop=author] > link[itemprop=url]").getAttribute("href").match(/www.youtube.com\/user\/([^\/]+)/)[1];
    }
   );
+  self.date = Try.all(
+   function() {
+    return new Date(document.getElementById("eow-date").textContent);
+   }
+  );
+  if (self.date)
+  {
+   self.day = ("0" + self.date.getDate()).match(/..$/)[0];
+   self.month = ("0" + (self.date.getMonth() + 1)).match(/..$/)[0];
+   self.year = self.date.getFullYear().toString();
+   self.date.toString = function() {
+    return self.year + "-" + self.month + "-" + self.day;
+   };
+  }
  }
  return self;
 })();
