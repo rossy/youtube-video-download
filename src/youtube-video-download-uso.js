f1e60ab75a8454ca313fdb2339c7d0f6ff26b638
James Ross-Gowan <rossymiles@gmail.com>
2012-12-30 23:07:06 +1100

    Add copyright notices

--- src/youtube-video-download-uso.js
+++ src/youtube-video-download-uso.js
@@ -14,6 +14,42 @@
 // @include        http://*.c.youtube.com/videoplayback?*
 // ==/UserScript==
 
+/*
+ * This file is a part of YouTube Video Download, which has been placed under
+ * the MIT/X11 license.
+ *
+ * Copyright (c) 2012, James Ross-Gowan and other contributors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/*
+ * Feel free to read and modify this source code, however please note that this
+ * is the compiled/pre-processed version of the script. It has been generated
+ * from several files and most whitespace has been removed. For easier reading,
+ * see the GitHub repository:
+ * https://github.com/rossy2401/youtube-video-download/tree/stable/src
+ *
+ * For translations, language files can be found here:
+ * https://github.com/rossy2401/youtube-video-download/tree/stable/lang
+ */
+
 (function() {
 	"use strict";
 
